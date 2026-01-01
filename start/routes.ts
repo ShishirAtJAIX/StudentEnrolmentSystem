b85225587b83a3410495ea014a623c9d5745e714
SHISHIR <shishir@jaix.com.au>
2026-01-01 16:03:47 +1030

    First Endpoints in the Adonis Project #1
    Endpoint#1

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,35 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import LecturersController from '#controllers/lecturers_controller'
 
+//root endpoint for the API
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+//endpoint for ABOUT page in the website
+router.get('/about', async () => {
+  return 'This is the about page.'
+})
+
+//endpoint for ABOUT page in the website
+router.get('/posts/:id', ({ params }) => {
+  return 'This is the post with the id ${params.id}'     //params didnt work 
+})
+
+//CRUD endpoints for the LecturersController
+router.group(() => {
+  router.resource('lecturers', LecturersController).apiOnly()
+})
+.prefix('/api/v1')
+
+// test endpoint to see if it works. 
+router.get('/test1', async () => {
+  return {
+    This: 'RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR',
+  }
+})
+
